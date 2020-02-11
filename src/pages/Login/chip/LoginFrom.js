866362d8efd280192e975f0c5b3e533d3dd67d4f
linghushuncheng <linghushuncheng@cetccloud.com>
2020-02-12 02:39:43 +0800

    新增登录页

--- src/pages/Login/chip/LoginFrom.js
+++ src/pages/Login/chip/LoginFrom.js
@@ -14,31 +14,44 @@ export default class LoginForm extends React.Component {
           console.log(values)
           return false
         }}
-        initValues={{
-          username: 'test'
-        }}
+        initValues={
+          {
+            // username: 'test'
+          }
+        }
         {...this.props}
       >
-        <Form.Item prop="username" rules={[required]}>
+        <Form.Item
+          prop="username"
+          rules={[required]}
+          wrapperCol={{ sm: { span: 24 } }}
+        >
           <Input
             prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-            placeholder="Username"
+            placeholder="用户名"
           />
         </Form.Item>
-        <Form.Item prop="reusername">
+        <Form.Item prop="password" wrapperCol={{ sm: { span: 24 } }}>
           <Input
             prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
             type="password"
-            placeholder="Password"
+            placeholder="密码"
           />
         </Form.Item>
-        <Form.Item>
+        <Form.Item prop="usbkey" wrapperCol={{ sm: { span: 24 } }}>
+          <Input
+            prefix={<Icon type="usb" style={{ color: 'rgba(0,0,0,.25)' }} />}
+            type="password"
+            placeholder="UsbKey"
+          />
+        </Form.Item>
+        <Form.Item wrapperCol={{ sm: { span: 24 } }}>
           <Button
             type="primary"
             htmlType="submit"
             className="login-form-button"
           >
-            提交
+            登录
           </Button>
         </Form.Item>
       </Formx>
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react'
 import LoginFrom from './chip/LoginFrom'
+import loginImg from '@/assets/login-wrap-bg.png'
 import './login.scss'
 
 export default class Login extends Component {
   render() {
     return (
       <div className="wrap">
-        <h3>DMS</h3>
-        <LoginFrom className="login-form" />
-        <p>欢迎登陆电科云-桌面管理系统</p>
+        <div className="content">
+          <div className="content-left">
+            <img src={loginImg} />
+          </div>
+          <div className="content-right">
+            <div className="login-title">安全云桌面系统</div>
+            <LoginFrom className="login-form" />
+            <div className="company-info">
+              <span className="company-logo"></span>
+              <p>
+                ————&nbsp;&nbsp;&nbsp;电科云（北京）有限公司&nbsp;&nbsp;&nbsp;————
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }

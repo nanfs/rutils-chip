0b853312d842ca3df710601401ff84d064400409
linghushuncheng <linghushuncheng@cetccloud.com>
2020-02-12 16:28:22 +0800

    【新增】Dashboard页面

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,25 +1,12 @@
 import React from 'react'
 import { Tabs } from 'antd'
-import TabpanTablex from './chip/TabpanTablex'
-import TabpanFormx from './chip/TabpanFormx'
-import TabpanModalx from './chip/TabpanModalx'
 
 const { TabPane } = Tabs
 class Dashboard extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Tabs tabPosition="left">
-          <TabPane tab="表格demo" key="tablbx">
-            <TabpanTablex />
-          </TabPane>
-          <TabPane tab="表单demo" key="formx">
-            <TabpanFormx />
-          </TabPane>
-          <TabPane tab="弹窗demo" key="modalx">
-            <TabpanModalx />
-          </TabPane>
-        </Tabs>
+        
       </React.Fragment>
     )
   }

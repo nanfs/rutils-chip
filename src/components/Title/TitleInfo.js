bdc1bb881b4f40ff24cc7049fe2594ff2166c290
linghushuncheng <linghushuncheng@cetccloud.com>
2020-02-13 03:36:27 +0800

    【修改】TitleInfo组件 添加右侧更多链接

--- src/components/Title/TitleInfo.js
+++ src/components/Title/TitleInfo.js
@@ -2,11 +2,22 @@ import React from 'react'
 import { Icon } from 'antd'
 
 export default function TitleInfo(props) {
-  const { slot } = props
+  const { slot, more } = props
   return (
     <p style={{ fontSize: '18px', fontWeight: 'bold' }}>
       <Icon type="info" />
       <span>{slot}</span>
+      <span
+        style={{
+          float: 'right',
+          paddingRight: 10,
+          fontWeight: 'normal',
+          fontSize: '14px',
+          lineHeight: '26px'
+        }}
+      >
+        {more}
+      </span>
     </p>
   )
 }

63b71ed7e3d6369eb8d99505dad22c1f4b3ca2ed
李帅 <nanfangshuai@foxmai.com>
2020-02-18 21:08:23 +0800

    [修改] 首页白条

--- src/pages/Login/chip/ftusbkey.js
+++ src/pages/Login/chip/ftusbkey.js
@@ -25,7 +25,7 @@ function ftInit() {
     const ctrl = document.createElement('object')
     ctrl.setAttribute('type', 'application/npFTUSBKeyMGR')
     ctrl.setAttribute('id', 'USBKeyMGR')
-    ctrl.setAttribute('style', 'width:0;height:0')
+    ctrl.setAttribute('style', 'width:0;height:0;display:none')
     document.body.appendChild(ctrl)
   }
 }

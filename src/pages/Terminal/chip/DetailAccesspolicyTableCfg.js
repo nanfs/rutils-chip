ecce25d35627aef8d7c7f509d724a246d53774e4
linghushuncheng <linghushuncheng@cetccloud.com>
2020-04-03 09:11:28 +0800

    【修改】终端管理详情 去掉筛选

--- src/pages/Terminal/chip/DetailAccesspolicyTableCfg.js
+++ src/pages/Terminal/chip/DetailAccesspolicyTableCfg.js
@@ -25,7 +25,6 @@ export const DetailAccesspolicyColumns = [
   {
     title: '准入类型',
     dataIndex: 'type',
-    filters: typeOptions,
     render: (_text, record) => {
       return record.admitInterval && typeArr[record.admitInterval[0].type]
     }

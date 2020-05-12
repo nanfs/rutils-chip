ca2ba1835399d2aa153bcff1222dd2bb5142763c
linghushuncheng <linghushuncheng@cetccloud.com>
2020-05-12 17:08:48 +0800

    【修改】资源概览 拓扑图数据联调

--- src/pages/Resource/index.js
+++ src/pages/Resource/index.js
@@ -6,19 +6,42 @@ import TableSave from './chip/TableSave'
 import Topology from './chip/Topology'
 
 import './index.less'
+import { Icon } from 'antd'
 
 export default class Resource extends React.Component {
   state = {
-    innerPath: undefined
+    innerPath: undefined,
+    viewType: true
+  }
+
+  viewChange() {
+    const viewType = !this.state.viewType
+    this.setState({ viewType })
   }
 
   render() {
     return (
       <React.Fragment>
+        <span
+          onClick={() => this.viewChange()}
+          style={{
+            float: 'right',
+            marginRight: 30,
+            marginTop: 20,
+            cursor: 'pointer'
+          }}
+        >
+          <Icon type="swap" style={{ color: '#1990ff' }} />
+          切换视图
+        </span>
         <InnerPath location="资源概览" inner={this.state.inner} />
-        <TableCompute></TableCompute>
-        <TableSave></TableSave>
-        {/* <Topology></Topology> */}
+        {this.state.viewType && (
+          <div>
+            <TableCompute></TableCompute>
+            <TableSave></TableSave>
+          </div>
+        )}
+        {!this.state.viewType && <Topology></Topology>}
       </React.Fragment>
     )
   }

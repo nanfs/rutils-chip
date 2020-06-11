b3f0144511cb7d1239f325379fc586767df19aba
linghushuncheng <linghushuncheng@cetccloud.com>
2020-06-11 18:21:55 +0800

    【修改】密码加密 覆盖上次提交

--- src/pages/Login/chip/LoginFrom.js
+++ src/pages/Login/chip/LoginFrom.js
@@ -68,8 +68,7 @@ export default class LoginForm extends React.Component {
     }
     data = {
       username: values.username,
-      // password: encrypt(values.password),
-      password: values.password,
+      password: encrypt(values.password),
       domain: 'internal'
     }
     loginApi

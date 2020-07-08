d9bf651a18d8c9fb0ff433eae3c2663c1b839e44
李帅 <lishuai@cetccloud.com>
2020-07-08 09:37:15 +0800

    access hooks 写法

--- src/pages/Access/index.js
+++ src/pages/Access/index.js
@@ -1,6 +1,5 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { Button, message, Modal, notification } from 'antd'
-import produce from 'immer'
 import { Tablex, InnerPath } from '@/components'
 import { wrapResponse } from '@/utils/tool'
 import accessApi from '@/services/access'
@@ -10,8 +9,8 @@ import { columns, apiMethod } from './chip/TableCfg'
 
 const { confirm } = Modal
 const { createTableCfg, TableWrap, ToolBar, BarLeft } = Tablex
-export default class Access extends React.Component {
-  name = {
+const Access = props => {
+  const cloName = {
     title: () => <span title="名称">名称</span>,
     width: 250,
     dataIndex: 'name',
@@ -19,17 +18,17 @@ export default class Access extends React.Component {
     sorter: true
   }
 
-  action = {
+  const action = {
     title: '操作',
     dataIndex: 'opration',
     width: 120,
     render: (text, record) => {
       return (
         <span className="opration-btn">
-          <a onClick={() => this.editAccess(record.name, record)}>编辑</a>
+          <a onClick={() => editAccess(record.name, record)}>编辑</a>
           <a
             onClick={() => {
-              this.delAccess(record.id, '确定删除该条数据?')
+              delAccess(record.id, '确定删除该条数据?')
             }}
             disabled={record.boundTcNum !== 0}
           >
@@ -40,15 +39,24 @@ export default class Access extends React.Component {
     }
   }
 
-  state = {
-    tableCfg: createTableCfg({
-      columns: [this.name, ...columns, this.action],
+  const tableRef = useRef()
+
+  const [tableCfg, setTableCfg] = useState(
+    createTableCfg({
+      columns: [cloName, ...columns, action],
       apiMethod
     })
-  }
+  )
+
+  const [disabled, setDisabled] = useState({})
+  const [inner, setInner] = useState('')
+
+  useEffect(() => {
+    tableRef.current.search(tableCfg)
+  }, [tableCfg])
 
   // 删除禁用 如果 有终端使用不能删除 建议放开 实现双向解绑
-  onSelectChange = (selection, selectData) => {
+  const onSelectChange = (selection, selectData) => {
     let disabledButton = {}
     if (selection.length === 0) {
       disabledButton = { ...disabledButton, disabledDelete: true }
@@ -58,11 +66,11 @@ export default class Access extends React.Component {
         disabledButton = { ...disabledButton, disabledDelete: true }
       }
     })
-    this.setState({ disabledButton })
+    setDisabled({ disabledButton })
   }
 
   // 支持对名称进行排序
-  onTableChange = (page, filter, sorter) => {
+  const onTableChange = (page, filter, sorter) => {
     const searchs = {}
     const orderArr = {
       ascend: 'asc',
@@ -76,42 +84,39 @@ export default class Access extends React.Component {
     if (filter) {
       searchs.type = filter.type
     }
-    this.setState(
-      produce(draft => {
-        draft.tableCfg.searchs = {
-          ...draft.tableCfg.searchs,
-          ...searchs
-        }
-      }),
-      () => this.tablex.search(this.state.tableCfg)
-    )
+    setTableCfg({ ...tableCfg, searchs: { ...tableCfg.searchs, ...searchs } })
+    // () => this.tablex.search(this.state.tableCfg)
   }
 
   // 返回调用
-  onBack = () => {
-    this.setState({ inner: undefined })
-    this.currentDrawer.drawer.hide()
+  const onBack = () => {
+    setInner()
+    // this.currentDrawer.drawer.hide()
   }
 
   // 内页成功执行后调用
-  onSuccess = () => {
+  const onSuccess = () => {
     this.onBack()
-    this.tablex.refresh(this.state.tableCfg)
+    // this.tablex.refresh(this.state.tableCfg)
   }
 
-  addAccess = () => {
-    this.setState({ inner: '创建' }, this.addDrawer.pop())
-    this.currentDrawer = this.addDrawer
+  const addAccess = () => {
+    setInner('创建')
+    // this.addDrawer.pop()
+    // this.setState({ inner: '创建' }, this.addDrawer.pop())
+    // this.currentDrawer = this.addDrawer
   }
 
   // 准入策略数据( 暂时没有通过单独接口调 直接通过列表取)
-  editAccess = (name, data) => {
-    this.setState({ inner: name }, this.editDrawer.pop(data))
-    this.currentDrawer = this.editDrawer
+  const editAccess = (name, data) => {
+    setInner(name)
+    // this.editDrawer.pop(data)
+    // this.setState({ inner: name }, this.editDrawer.pop(data))
+    // this.currentDrawer = this.editDrawer
   }
 
   // 删除准入策略 可以通过工具条 和表单操作列传入操作 表单操作列传入为单个
-  delAccess = (id, title = '确定删除所选数据?') => {
+  const delAccess = (id, title = '确定删除所选数据?') => {
     const ids = Array.isArray(id) ? [...id] : [id]
     confirm({
       title,
@@ -121,7 +126,7 @@ export default class Access extends React.Component {
           .then(res =>
             wrapResponse(res).then(() => {
               notification.success({ message: '删除成功' })
-              this.tablex.refresh(this.state.tableCfg)
+              // this.tablex.refresh(tableCfg)
             })
           )
           .catch(error => {
@@ -132,53 +137,42 @@ export default class Access extends React.Component {
     })
   }
 
-  render() {
-    const { disabledButton } = this.state
-    return (
-      <React.Fragment>
-        <InnerPath
-          location="准入控制"
-          inner={this.state.inner}
-          onBack={this.onBack}
+  return (
+    <React.Fragment>
+      <InnerPath location="准入控制" inner={inner} onBack={onBack} />
+      <TableWrap>
+        <ToolBar>
+          <BarLeft>
+            <Button onClick={addAccess} type="primary">
+              创建
+            </Button>
+            <Button
+              onClick={() => delAccess(tableRef.current.getSelection())}
+              disabled={disabled?.disabledDelete}
+            >
+              删除
+            </Button>
+          </BarLeft>
+        </ToolBar>
+        <Tablex
+          ref={tableRef}
+          onSelectChange={onSelectChange}
+          tableCfg={tableCfg}
+          onChange={onTableChange}
         />
-        <TableWrap>
-          <ToolBar>
-            <BarLeft>
-              <Button onClick={this.addAccess} type="primary">
-                创建
-              </Button>
-              <Button
-                onClick={() => this.delAccess(this.tablex.getSelection())}
-                disabled={disabledButton?.disabledDelete}
-              >
-                删除
-              </Button>
-            </BarLeft>
-          </ToolBar>
-          <Tablex
-            onRef={ref => {
-              this.tablex = ref
-            }}
-            onSelectChange={this.onSelectChange}
-            tableCfg={this.state.tableCfg}
-            onChange={this.onTableChange}
-          />
-          <AddDrawer
-            onRef={ref => {
-              this.addDrawer = ref
-            }}
-            onSuccess={this.onSuccess}
-            onClose={this.onBack}
-          />
-          <EditDrawer
-            onRef={ref => {
-              this.editDrawer = ref
-            }}
-            onClose={this.onBack}
-            onSuccess={this.onSuccess}
-          />
-        </TableWrap>
-      </React.Fragment>
-    )
-  }
+        <AddDrawer
+          ref={addDrawer}
+          onSuccess={this.onSuccess}
+          onClose={this.onBack}
+        />
+        <EditDrawer
+          ref={editDrawer}
+          onClose={this.onBack}
+          onSuccess={this.onSuccess}
+        />
+      </TableWrap>
+    </React.Fragment>
+  )
 }
+
+export default Access

6e25cda6b26d2209ae7bf6631a63e832d7a5c7b3
李帅 <lishuai@cetccloud.com>
2020-08-07 16:01:00 +0800

    [添加] 添加桌面 操作系统选项

---
+++ src/utils/enum.js
@@ -0,0 +1,444 @@
+// 操作系统os 码值
+export const osType = [
+  {
+    id: '0',
+    name: 'Other OS',
+    icon: 'os-other',
+    text: 'Other OS'
+  },
+  {
+    id: '1',
+    name: 'Windows XP',
+    icon: 'os-windows',
+    text: 'Win'
+  },
+  {
+    id: '3',
+    name: 'Windows 2003',
+    icon: 'os-windows',
+    text: 'Win'
+  },
+  {
+    id: '4',
+    name: 'Windows 2008',
+    icon: 'os-windows',
+    text: 'Win'
+  },
+
+  {
+    id: '5',
+    name: 'Linux',
+    icon: 'os-linux',
+    text: 'linux'
+  },
+  {
+    id: '7',
+    name: 'Red Hat Enterprise Linux 5.x',
+    icon: 'os-redhat',
+    text: '红帽'
+  },
+  {
+    id: '8',
+    name: 'Red Hat Enterprise Linux 4.x',
+    icon: 'os-redhat',
+    text: '红帽'
+  },
+  {
+    id: '9',
+    name: 'Red Hat Enterprise Linux 3.x',
+    icon: 'os-redhat',
+    text: '红帽'
+  },
+  {
+    id: '10',
+    name: 'Windows 2003 x64',
+    icon: 'os-windows',
+    text: 'Win'
+  },
+  {
+    id: '11',
+    name: 'Windows 7',
+    icon: 'os-windows',
+    text: 'Win'
+  },
+  {
+    id: '12',
+    name: 'Windows 7 x64',
+    icon: 'os-windows',
+    text: 'Win'
+  },
+  {
+    id: '13',
+    name: 'Red Hat Enterprise Linux 5.x x64',
+    icon: 'os-redhat',
+    text: '红帽'
+  },
+  {
+    id: '14',
+    name: 'Red Hat Enterprise Linux 4.x x64',
+    icon: 'os-redhat',
+    text: '红帽'
+  },
+  {
+    id: '15',
+    name: 'Red Hat Enterprise Linux 3.x x64',
+    icon: 'os-redhat',
+    text: '红帽'
+  },
+  {
+    id: '16',
+    name: 'Windows 2008 x64',
+    icon: 'os-windows',
+    text: 'Win'
+  },
+  {
+    id: '17',
+    name: 'Windows 2008 R2 x64',
+    icon: 'os-windows',
+    text: 'Win'
+  },
+  {
+    id: '18',
+    name: 'Red Hat Enterprise Linux 6.x',
+    icon: 'os-redhat',
+    text: '红帽'
+  },
+
+  {
+    id: '19',
+    name: 'Red Hat Enterprise Linux 6.x x64',
+    icon: 'os-redhat',
+    text: '红帽'
+  },
+  {
+    id: '20',
+    name: 'Windows 8',
+    icon: 'os-windows',
+    text: 'Win'
+  },
+  {
+    id: '21',
+    name: 'Windows 8 x64',
+    icon: 'os-windows',
+    text: 'Win'
+  },
+  {
+    id: '23',
+    name: 'Windows 2012 x64',
+    icon: 'os-windows',
+    text: 'Win'
+  },
+  {
+    id: '24',
+    name: 'Red Hat Enterprise Linux 7.x x64',
+    icon: 'os-redhat',
+    text: '红帽'
+  },
+  {
+    id: '25',
+    name: 'Windows 2012R2 x64',
+    icon: 'os-windows',
+    text: 'Win'
+  },
+  {
+    id: '26',
+    name: 'Windows 10',
+    icon: 'os-windows',
+    text: 'Win'
+  },
+  {
+    id: '27',
+    name: 'Windows 10 x64',
+    icon: 'os-windows',
+    text: 'Win'
+  },
+  { id: '28', name: 'Red Hat Atomic 7.x x64', icon: 'os-redhat', text: '红帽' },
+  {
+    id: '29',
+    name: 'Windows 2016 x64',
+    icon: 'os-windows',
+    text: 'Win'
+  },
+  {
+    id: '30',
+    name: 'Red Hat Enterprise Linux 8.x x64',
+    icon: 'os-redhat',
+    text: '红帽'
+  },
+  {
+    id: '31',
+    name: 'Windows 2019 x64',
+    icon: 'os-windows',
+    text: 'Win'
+  },
+  {
+    id: '33',
+    name: 'Other Linux (kernel 4.x)',
+    icon: 'os-linux',
+    text: 'linux'
+  },
+  {
+    id: '35',
+    name: 'Red Hat Enterprise Linux CoreOS',
+    icon: 'os-linux',
+    text: 'linux'
+  },
+  { id: '50', name: 'AARCH OS', icon: 'os-other', text: 'Other OS' },
+  { id: '51', name: '', icon: 'os-other', text: 'Other OS' },
+  {
+    id: '60',
+    name: 'YinHe Kylin Desktop System 4.x x64',
+    icon: 'os-kylin',
+    text: '麒麟'
+  },
+  {
+    id: '61',
+    name: 'iSoft Desktop System 5.x x64',
+    icon: 'os-puhua',
+    text: '普华'
+  },
+  {
+    id: '62',
+    name: 'Deepin Desktop System 15.x x64',
+    icon: 'os-linux',
+    text: '深度'
+  },
+  {
+    id: '100',
+    name: 'YinHe Kylin Desktop System 4.x x64',
+    icon: 'os-kylin',
+    text: '麒麟'
+  },
+  {
+    id: '105',
+    name: 'NEO Kylin Desktop System 7.x x64',
+    icon: 'os-kylin',
+    text: '麒麟'
+  },
+  {
+    id: '110',
+    name: 'iSoft Desktop System 5.x x64',
+    icon: 'os-puhua',
+    text: 'puhua'
+  },
+  {
+    id: '120',
+    name: 'Deepin Desktop System 15.x x64',
+    icon: 'os-linux',
+    text: '深度'
+  },
+  {
+    id: '130',
+    name: 'UOS Desktop System 2.x x64',
+    icon: 'os-linux',
+    text: 'UOS'
+  },
+  {
+    id: '140',
+    name: 'EulerOS Desktop System 2.x x64',
+    icon: 'os-linux',
+    text: 'EulerOS'
+  },
+  {
+    id: '1001',
+    name: 'Other OS',
+    icon: 'os-linux',
+    text: 'Other OS'
+  },
+  { id: '1002', name: 'Linux', icon: 'os-linux', text: 'linux' },
+  {
+    id: '1003',
+    name: 'Red Hat Enterprise Linux up to 6.8',
+    icon: 'os-redhat',
+    text: '红帽'
+  },
+  {
+    id: '1004',
+    name: 'SUSE Linux Enterprise Server 11',
+    icon: 'os-linux',
+    text: 'linux'
+  },
+  {
+    id: '1005',
+    name: 'Ubuntu Trusty Tahr LTS',
+    icon: 'os-ubuntu',
+    text: '乌班图'
+  },
+  {
+    id: '1006',
+    name: 'Red Hat Enterprise Linux 7.x',
+    icon: 'os-redhat',
+    text: '红帽'
+  },
+  {
+    id: '1007',
+    name: 'Red Hat Enterprise Linux 6.9+',
+    icon: 'os-redhat',
+    text: '红帽'
+  },
+  {
+    id: '1193',
+    name: 'SUSE Linux Enterprise Server 11+',
+    icon: 'os-linux',
+    text: 'linux'
+  },
+
+  {
+    id: '1252',
+    name: 'Ubuntu Precise Pangolin LTS',
+    icon: 'os-ubuntu',
+    text: '乌班图'
+  },
+  {
+    id: '1253',
+    name: 'Ubuntu Quantal Quetzal',
+    icon: 'os-ubuntu',
+    text: '乌班图'
+  },
+  {
+    id: '1254',
+    name: 'Ubuntu Raring Ringtails',
+    icon: 'os-ubuntu',
+    text: '乌班图'
+  },
+  {
+    id: '1255',
+    name: 'Ubuntu Saucy Salamander',
+    icon: 'os-ubuntu',
+    text: '乌班图'
+  },
+  {
+    id: '1256',
+    name: 'Ubuntu Trusty Tahr LTS',
+    icon: 'os-ubuntu',
+    text: '乌班图'
+  },
+
+  { id: '1300', name: 'Debian 7', icon: 'os-linux', text: 'Debian' },
+  { id: '1500', name: 'FreeBSD 9.2', icon: 'os-linux', text: 'linux' },
+  { id: '1501', name: 'FreeBSD 9.2 x64', icon: 'os-linux', text: 'linux' },
+  { id: '2002', name: 'Other OS', icon: 'os-linux', text: 'linux' },
+  { id: '2002', name: 'Linux', icon: 'os-linux', text: 'linux' },
+  {
+    id: '2003',
+    name: 'Red Hat Enterprise Linux 7.x',
+    icon: 'os-redhat',
+    text: '红帽'
+  },
+  {
+    id: '2004',
+    name: 'SUSE Linux Enterprise Server 12',
+    icon: 'os-linux',
+    text: 'linux'
+  },
+  {
+    id: '2005',
+    name: 'Ubuntu Xenial Xerus LTS',
+    icon: 'os-ubuntu',
+    text: '乌班图'
+  }
+]
+export const vmStatus = [
+  {
+    status: '-1',
+    name: 'vm-unassigned',
+    icon: 'vm-unassigned',
+    text: '未指派的'
+  },
+  {
+    status: '0',
+    name: 'vm-down',
+    icon: 'vm-down',
+    text: '已关机'
+  },
+  {
+    status: '1',
+    name: 'vm-up',
+    icon: 'vm-up',
+    text: '已开机'
+  },
+  {
+    status: '2',
+    name: 'vm-poweringup',
+    icon: 'vm-poweringup',
+    text: '正在开机'
+  },
+  {
+    status: '4',
+    name: 'vm-paused',
+    icon: 'vm-paused',
+    text: '暂停'
+  },
+  {
+    status: '5',
+    name: 'vm-migratingfrom',
+    icon: 'vm-migratingfrom',
+    text: '准备迁移'
+  },
+  {
+    status: '6',
+    name: 'vm-migratingTo',
+    icon: 'vm-migratingTo',
+    text: '迁移中'
+  },
+  {
+    status: '7',
+    name: 'vm-unknown',
+    icon: 'vm-unknown',
+    text: '未知'
+  },
+  {
+    status: '8',
+    name: 'vm-nonresponsive',
+    icon: 'vm-nonresponsive',
+    text: '没有响应'
+  },
+  {
+    status: '9',
+    name: 'vm-waitforlaunch',
+    icon: 'vm-waitforlaunch',
+    text: '等待'
+  },
+  {
+    status: '10',
+    name: 'vm-rebootinprogress',
+    icon: 'vm-rebootinprogress',
+    text: '重启过程中'
+  },
+  {
+    status: '11',
+    name: 'vm-savingstate',
+    icon: 'vm-savingstate',
+    text: '保存状态'
+  },
+  {
+    status: '12',
+    name: 'vm-restoringstate',
+    icon: 'vm-restoringstate',
+    text: '恢复状态'
+  },
+  {
+    status: '13',
+    name: 'vm-suspended',
+    icon: 'vm-suspended',
+    text: '挂起'
+  },
+  {
+    status: '14',
+    name: 'vm-ImageIllegal',
+    icon: 'vm-ImageIllegal',
+    text: '镜像损坏'
+  },
+  {
+    status: '15',
+    name: 'vm-imagelocked',
+    icon: 'vm-imagelocked',
+    text: '镜像锁定'
+  },
+  {
+    status: '16',
+    name: 'vm-poweringdown',
+    icon: 'vm-poweringdown',
+    text: '正在关机'
+  }
+]
